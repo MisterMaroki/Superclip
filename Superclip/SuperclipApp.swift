817fca957565501d5534350b90e0fd8ca2a20923
MisterMaroki <omar987@hotmail.co.uk>
2026-01-23 23:24:10 +0000

    Initial Commit

---
+++ Superclip/SuperclipApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuperclipApp.swift
+//  Superclip
+//
+//  Created by Omar Maroki on 23/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct SuperclipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
